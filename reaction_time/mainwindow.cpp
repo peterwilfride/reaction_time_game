119fdb859d25b5b19e1c2c66f0f72e16f7ec2684
peterwilfride <cybermind545@gmail.com>
2021-11-27 22:00:20 -0300

    add all project files

---
+++ reaction_time/mainwindow.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter shape(this);
+
+    std::uniform_int_distribution<int> width_dist(0+size+10, 570-size+10);
+    std::uniform_int_distribution<int> height_dist(0+size+10, 440-size+10);
+    std::uniform_int_distribution<int> color_dist(0, 3);
+
+    int rand_x = width_dist(*QRandomGenerator::global());
+    int rand_y = height_dist(*QRandomGenerator::global());
+    int rand_c = color_dist(*QRandomGenerator::global());
+
+    shape.setRenderHint(QPainter::Antialiasing, true);
+    shape.drawRect(QRect(10, 10, 570, 440));
+
+    switch (rand_c) {
+        case 0:
+            shape.setBrush(QBrush(Qt::blue, Qt::SolidPattern));
+            break;
+        case 1:
+            shape.setBrush(QBrush(Qt::yellow, Qt::SolidPattern));
+            break;
+        case 2:
+            shape.setBrush(QBrush(Qt::red, Qt::SolidPattern));
+            break;
+        case 3:
+            shape.setBrush(QBrush(Qt::green, Qt::SolidPattern));
+            break;
+        default:
+            break;
+    }
+
+    shape.drawEllipse(rand_x, rand_y, size, size);
+
+}
+
---
+++ reaction_time/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QRandomGenerator>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    virtual void paintEvent(QPaintEvent *event);
+
+    int size = 100;
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
